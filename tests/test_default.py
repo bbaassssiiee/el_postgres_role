9793a2876d6b871ac1211f082c686580309645fd
Bas Meijer <bas.meijer@me.com>
2016-09-17 20:55:41 +0200

    pg tests

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,14 +4,12 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_hosts_file(File):
-    f = File('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
-
-
 def test_postgres_server_installed(Package):
     p = Package('postgresql94-server')
     assert p.is_installed
+
+
+def test_postgres_running_and_enabled(Service):
+    postgresql = Service("postgresql-9.4")
+    assert postgresql.is_running
+    assert postgresql.is_enabled
