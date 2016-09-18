5141b7b1d0c084cbfb201129c764c358c697deab
Bas Meijer <bas.meijer@me.com>
2016-09-18 18:19:42 +0200

    2 passed

--- tests/test_default.py
+++ tests/test_default.py
@@ -7,9 +7,3 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 def test_postgres_server_installed(Package):
     p = Package('postgresql94-server')
     assert p.is_installed
-
-
-def test_postgres_running_and_enabled(Service):
-    postgresql = Service("postgresql-9.4")
-    assert postgresql.is_running
-    assert postgresql.is_enabled
